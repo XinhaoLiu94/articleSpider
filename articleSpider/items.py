acfa4e372107e79b72c9d976721f670eb9d0900f
LIU Xinhao <liuxinhao@eisti.eu>
2019-03-26 23:32:46 +0100

    Update items

--- articleSpider/items.py
+++ articleSpider/items.py
@@ -12,3 +12,14 @@ class ArticlespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ArticleItem(scrapy.Item):
+    title = scrapy.Field()
+    create_date = scrapy.Field()
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    praise_nums = scrapy.Field()
+    comment_nums = scrapy.Field()
+    favour_nums = scrapy.Field()
+    tags = scrapy.Field()
+    content = scrapy.Field()
--- articleSpider/spiders/jobbole.py
+++ articleSpider/spiders/jobbole.py
@@ -2,6 +2,7 @@
 import scrapy
 import re
 from scrapy.http import Request
+from articleSpider.items import ArticleItem
 
 
 
@@ -23,23 +24,35 @@ class JobboleSpider(scrapy.Spider):
 
 
     def parse_detail(self,response):
-        re_selector = response.xpath('//*[@class="entry-header"]/h1/text()').extract()
+        article_item = ArticleItem()
+        title = response.xpath('//*[@class="entry-header"]/h1/text()').extract()
         create_date = response.xpath('//*[@class="entry-meta"]/p/text()').extract()[0].strip().replace("·"," ").strip()
         praise_numbers = int(response.xpath('//*[@class=" btn-bluet-bigger href-style vote-post-up   register-user-only "]/h10/text()').extract()[0])
         favor_numbers = response.xpath('//*[@class=" btn-bluet-bigger href-style bookmark-btn  register-user-only "]/text()').extract()[0]
         match_re = re.match(".*(\d+).*",favor_numbers)
         if match_re:
-            favor_numbers = match_re.group(1)
+            favor_numbers = int(match_re.group(1))
         else:
             favor_numbers = 0
 
         comment_numbers = response.xpath('//*[@class="btn-bluet-bigger href-style hide-on-480"]/text()').extract()[0]
         match_re = re.match(".*(d+).*",comment_numbers)
         if match_re:
-            comment_numbers = match_re.group(1)
+            comment_numbers = int(match_re.group(1))
         else:
             comment_numbers = 0
 
         content = response.xpath("//div[@class='entry']").extract()[0]
         taglist = response.xpath('//*[@class="entry-meta-hide-on-mobile"]/a/text()').extract()
+
+        article_item["title"] = title
+        article_item["url"] = response.url
+        article_item["create_date"] = create_date
+        article_item["praise_nums"] = praise_numbers
+        article_item["comment_nums"] = comment_numbers
+        article_item["favour_nums"] = favor_numbers
+        article_item["tags"] = taglist
+        article_item["content"] = content
+
+        yield article_item
         pass
