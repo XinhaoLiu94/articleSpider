3b60022a6f960c1d343ec51723147b9fe87fee3b
LIU Xinhao <liuxinhao@eisti.eu>
2019-03-28 00:01:14 +0100

    until mysql

--- articleSpider/spiders/jobbole.py
+++ articleSpider/spiders/jobbole.py
@@ -12,6 +12,7 @@ class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
     start_urls = ['http://blog.jobbole.com/all-posts/']
+    #######
 
     def parse(self, response):
         #获取列表页的
